60fbae8540b28f6b96714fb5fa137a6357c5d4c6
Thomas Bouron <thomas.bouron@ecoles-epsi.net>
2024-06-19 16:42:55 +0200

    correction cors site final

--- api/config/cors.js
+++ api/config/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: ['http://localhost:3000/api-docs/', 'https://ubeer-back-wgw6.onrender.com/api-docs/'],
+    origin: ['http://localhost:3000/api-docs', 'https://ubeer-back-wgw6.onrender.com/api-docs', 'https://ubeer.flutterflow.app'],
     optionsSuccessStatus: 200
 };
 
